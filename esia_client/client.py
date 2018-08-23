3443303ff7c8471bcb34aab66ed8a5634c2699e9
Чуркин Кирилл Сергеевич <briizzzz@mail.ru>
2018-08-23 17:54:43 +0500

    Pretty changes

--- esia_client/client.py
+++ esia_client/client.py
@@ -3,11 +3,15 @@ import uuid
 from typing import *
 
 import esia_client.utils
+import esia_client.exceptions
 
-__all__ = ['']
+__all__ = ['Settings', 'Scope', 'Auth']
 
 
 class Scope(enum.Enum):
+    """
+    Типы запросов авторизации и информации о пользователе в ЕСИА
+    """
     Authorization = 'openid'
     Fullname = 'fullname'
     Birthdate = 'birthdate'
@@ -36,6 +40,7 @@ class Settings:
             private_key_file: приватный ключ для генерации электронной подписи запроса
             esia_service_url: ссылка на стенд ЕСИА
             scopes: запрашиваемые разрешения на получение данных о пользователе
+
         """
         self.esia_client_id = esia_client_id
         self.redirect_uri = redirect_uri
@@ -49,122 +54,16 @@ class Settings:
         return ' '.join((str(x) for x in self.scopes))
 
 
-class Auth:
+class UserInfo:
     """
-    Клиент ЕСИА
+    Клиент получения пользовательских данных из ЕСИА
     """
-    # _ESIA_ISSUER_NAME = 'http://esia.gosuslugi.ru/'
-    _AUTHORIZATION_URL = '/aas/oauth2/ac'
-    _TOKEN_EXCHANGE_URL = '/aas/oauth2/te'
-
-    def __init__(self, settings: Settings):
+    def __init__(self, access_token: str, oid: str, settings: Settings):
         """
         Args:
+            access_token: токен авторизации
+            oid: идентификатор пользователя в системе ЕСИА
             settings: настройки клиента ЕСИА
-        """
-        self.settings = settings
-
-    def _sign_params(self, params: dict):
-        """
-        Подписывает параметры цифровой подписью.
-        Метод добавляет в параметры поле client_secret с закодированной в base64 цифровой подписью
-
-        Args:
-            params: параметры запроса
-
-        """
-        parts = (
-                str(params.get('scope', '')),
-                params.get('timestamp', ''),
-                params.get('client_id', ''),
-                str(params.get('state', '')),
-        )
-
-        params['client_sercet'] = esia_client.utils.sign_data(
-            ''.join(parts), cert_path=self.settings.cert_file, private_key_path=self.settings.private_key_file
-        )
-
-    def get_auth_url(self, state: Union[str, uuid.UUID] = uuid.uuid4(), redirect_uri=None):
-        """
-        Генерация URL перехода на сайт ЕСИА для авторизации пользователя
-        Args:
-            state: идентификатор запроса
-            redirect_uri: ссылка для перенаправления пользователя после авторизации
-
-        Returns:
-            Ссылка авторизации
-        """
-        params = {
-            'client_id': self.settings.esia_client_id,
-            'redirect_uri': redirect_uri or self.settings.redirect_uri,
-            'scope': self.settings.scope_string,
-            'response_type': 'code',
-            'state': state or str(uuid.uuid4()),
-            'timestamp': esia_client.utils.get_timestamp(),
-            'access_type': 'offline'
-        }
-        self._sign_params(params)
-
-        return '{base_url}{auth_url}?{params}'.format(base_url=self.settings.esia_service_url,
-                                                      auth_url=self._AUTHORIZATION_URL,
-                                                      params=esia_client.utils.format_uri_params(params))
-
-    def complete_authorization(self, code, state, redirect_uri=None):
-        """
-        Exchanges received code and state to access token, validates token (optionally), extracts ESIA user id from
-        token and returns ESIAInformationConnector instance.
-        :type code: str
-        :type state: str
-        :param boolean validate_token: perform token validation
-        :param str or None redirect_uri: uri, where browser will be redirected after authorization.
-        :rtype: EsiaInfo
-        :raises IncorrectJsonError: if response contains invalid json body
-        :raises HttpError: if response status code is not 2XX
-        :raises IncorrectMarkerError: if validate_token set to True and received token cannot be validated
-        """
-
-        params = {
-            'client_id': self.settings.esia_client_id,
-            'code': code,
-            'grant_type': 'authorization_code',
-            'redirect_uri': redirect_uri or self.settings.redirect_uri,
-            'timestamp': esia_client.utils.get_timestamp(),
-            'token_type': 'Bearer',
-            'scope': self.settings.scope_string,
-            'state': str(uuid.uuid4()),
-        }
-
-        self._sign_params(params)
-
-        response_json = esia_client.utils.make_request(
-            url=f"{self.settings.esia_service_url}{self._TOKEN_EXCHANGE_URL}", method='POST', data=params)
-
-        print(response_json)
-        access_token = response_json['access_token']
-        id_token = response_json['id_token']
-        payload = esia_client.utils.decode_payload(id_token.split('.')[1])
-
-        return EsiaInfo(access_token=access_token,
-                        oid=self._get_user_id(payload),
-                        settings=self.settings)
-
-    @staticmethod
-    def _get_user_id(payload):
-        """
-        :param dict payload: token payload
-        """
-        return payload.get('urn:esia:sbj', {}).get('urn:esia:sbj:oid')
-
-
-class EsiaInfo:
-    """
-    Connector for fetching information from ESIA REST services.
-    """
-    def __init__(self, access_token, oid, settings):
-        """
-        :param str access_token: access token
-        :param int oid: ESIA object id
-        :param Settings settings: connector settings
         """
         self.token = access_token
         self.oid = oid
@@ -223,4 +122,126 @@ class EsiaInfo:
         """
         url = '{base}/prns/{oid}/docs/{doc_id}'.format(base=self._rest_base_url, oid=self.oid, doc_id=doc_id)
         return self._request(url=url)
+
+
+class Auth:
+    """
+    Клиент авторизации ЕСИА
+    """
+    # _ESIA_ISSUER_NAME = 'http://esia.gosuslugi.ru/'
+    _AUTHORIZATION_URL = '/aas/oauth2/ac'
+    _TOKEN_EXCHANGE_URL = '/aas/oauth2/te'
+
+    def __init__(self, settings: Settings):
+        """
+        Args:
+            settings: настройки клиента ЕСИА
+        """
+        self.settings = settings
+
+    def _sign_params(self, params: dict):
+        """
+        Подписывает параметры цифровой подписью.
+        Метод добавляет в параметры поле client_secret с закодированной в base64 цифровой подписью
+
+        Args:
+            params: параметры запроса
+
+        """
+        parts = (
+                str(params.get('scope', '')),
+                params.get('timestamp', ''),
+                params.get('client_id', ''),
+                str(params.get('state', '')),
+        )
+
+        params['client_sercet'] = esia_client.utils.sign_data(
+            ''.join(parts), cert_path=self.settings.cert_file, private_key_path=self.settings.private_key_file
+        )
+
+    def get_auth_url(self, state: Union[str, uuid.UUID] = uuid.uuid4(), redirect_uri=None):
+        """
+        Генерация URL перехода на сайт ЕСИА для авторизации пользователя
+
+        Args:
+            state: идентификатор запроса
+            redirect_uri: ссылка для перенаправления пользователя после авторизации
+
+        Returns:
+            Ссылка авторизации
+        """
+        params = {
+            'client_id': self.settings.esia_client_id,
+            'redirect_uri': redirect_uri or self.settings.redirect_uri,
+            'scope': self.settings.scope_string,
+            'response_type': 'code',
+            'state': state or str(uuid.uuid4()),
+            'timestamp': esia_client.utils.get_timestamp(),
+            'access_type': 'offline'
+        }
+        self._sign_params(params)
+
+        return '{base_url}{auth_url}?{params}'.format(base_url=self.settings.esia_service_url,
+                                                      auth_url=self._AUTHORIZATION_URL,
+                                                      params=esia_client.utils.format_uri_params(params))
+
+    def complete_authorization(self, code, state: str = str(uuid.uuid4()), redirect_uri: str = None) -> UserInfo:
+        """
+        Полученение токена авторизации и клиента запроса информации
+
+        Args:
+            code: код авторизации
+            state: идентификатор сессии авторизации в формате `uuid.UUID`
+            redirect_uri: URL для переадресации после авторизации
+
+        Returns:
+
+        """
+
+        """
+        Exchanges received code and state to access token, validates token (optionally), extracts ESIA user id from
+        token and returns ESIAInformationConnector instance.
+        :type code: str
+        :type state: str
+        :param boolean validate_token: perform token validation
+        :param str or None redirect_uri: uri, where browser will be redirected after authorization.
+        :rtype: UserInfo
+        :raises IncorrectJsonError: if response contains invalid json body
+        :raises HttpError: if response status code is not 2XX
+        :raises IncorrectMarkerError: if validate_token set to True and received token cannot be validated
+        """
+
+        params = {
+            'client_id': self.settings.esia_client_id,
+            'code': code,
+            'grant_type': 'authorization_code',
+            'redirect_uri': redirect_uri or self.settings.redirect_uri,
+            'timestamp': esia_client.utils.get_timestamp(),
+            'token_type': 'Bearer',
+            'scope': self.settings.scope_string,
+            'state': state,
+        }
+
+        self._sign_params(params)
+
+        response_json = esia_client.utils.make_request(
+            url=f"{self.settings.esia_service_url}{self._TOKEN_EXCHANGE_URL}", method='POST', data=params)
+
+        access_token = response_json['access_token']
+        id_token = response_json['id_token']
+        payload = esia_client.utils.decode_payload(id_token.split('.')[1])
+
+        return UserInfo(access_token=access_token,
+                        oid=self._get_user_id(payload),
+                        settings=self.settings)
+
+    @staticmethod
+    def _get_user_id(payload: dict) -> str:
+        """
+        Получение идентификатора пользователя в системе ЕСИА из данных токена
+        """
+        try:
+            return payload['urn:esia:sbj']['urn:esia:sbj:oid']
+        except KeyError:
+            raise esia_client.exceptions.IncorrectMarkerError(payload)
 
