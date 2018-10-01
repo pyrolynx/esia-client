11748aa01eb4d2d7a22bb3189b2ef2d841bfa808
Чуркин Кирилл Сергеевич <briizzzz@mail.ru>
2018-10-01 12:43:27 +0500

    Renew origin

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+
+install_requires = ['pytz==2015.7', 'requests==2.8.1', 'pyOpenSSL==18.0.0']
+
+
+setup(
+    name='esia-client',
+    version='0.2',
+    description='Клиент для авторизации через ЕСИА',
+    author='Kirill Churkin',
+    author_email='briizzzz@mail.ru',
+    url='https://github.com/pyrolynx/esia-client',
+    license='BSD',
+    packages=['esia_client'],
+    install_requires=install_requires,
+)
